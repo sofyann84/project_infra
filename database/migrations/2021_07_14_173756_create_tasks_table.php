10d79a6612496bfcd5ab5097495f7da467248356
sofyann84 <sofyan.dbx@gmail.com>
2021-07-15 01:07:19 +0700

    migration done

---
+++ database/migrations/2021_07_14_173756_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('taskname');
+            $table->string('vendor');
+            $table->foreignId('siteid');
+            $table->timestamps();
+            $table->foreign('siteid')->references('id')->on('sites');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
